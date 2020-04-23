59c814ff19597151e0926015b7580186af62a329
Prithaj Nath <prithajnath@gmail.com>
2020-04-22 20:19:57 -0400

    Follow style guidelines

--- main.go
+++ main.go
@@ -1,68 +1,68 @@
 package main
 
-import(
-  "fmt"
-  "context"
-  "io"
-  "os"
-  "sync"
-  "./runtime"
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"sync"
+
+	"./runtime"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 )
 
-
 var ctx context.Context = context.Background()
 var wg sync.WaitGroup
 
-func createContainer(cli *client.Client, r runtime.Runtime){
+func createContainer(cli *client.Client, r runtime.Runtime) {
 
-  if r.CheckIfExistsLocally(cli, ctx) != true{
-    fmt.Printf("%s not found in host. Pulling from container registry...\n", r.Tag)
-    out, pullError := cli.ImagePull(ctx, r.Tag, types.ImagePullOptions{All: false})
-    if pullError != nil{
-      panic(pullError)
-    }
+	if r.CheckIfExistsLocally(ctx, cli) != true {
+		fmt.Printf("%s not found in host. Pulling from container registry...\n", r.Tag)
+		out, pullError := cli.ImagePull(ctx, r.Tag, types.ImagePullOptions{All: false})
+		if pullError != nil {
+			panic(pullError)
+		}
 
-    defer out.Close()
-    io.Copy(os.Stdout, out)
-  }
+		defer out.Close()
+		io.Copy(os.Stdout, out)
+	}
 
-  resp, err := cli.ContainerCreate(ctx, &container.Config{
-    Image: r.Image,
-    Cmd: []string{"/bin/bash"},
-    Tty: true,
-  }, nil, nil, r.Name)
-  if err != nil {
-    panic(err)
-  }
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: r.Image,
+		Cmd:   []string{"/bin/bash"},
+		Tty:   true,
+	}, nil, nil, r.Name)
+	if err != nil {
+		panic(err)
+	}
 
-  if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-    panic(err)
-  }
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
 
-  fmt.Printf("Created container %s : %s\n", r.Name, resp.ID)
-  wg.Done()
+	fmt.Printf("Created container %s : %s\n", r.Name, resp.ID)
+	wg.Done()
 }
 
-func runCode(r runtime.Runtime, code string){
+func runCode(r runtime.Runtime, code string) {
 
 }
 
-func main(){
+func main() {
 
-  cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-  if err != nil {
-    panic(err)
-  }
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
 
-  wg.Add(len(runtime.Runtimes))
+	wg.Add(len(runtime.Runtimes))
 
-  for _, r := range runtime.Runtimes{
-    go createContainer(cli, r)
-  }
+	for _, r := range runtime.Runtimes {
+		go createContainer(cli, r)
+	}
 
-  wg.Wait()
+	wg.Wait()
 
 }
--- runtime/runtime.go
+++ runtime/runtime.go
@@ -1,58 +1,61 @@
-// Runtimes for different programming languages
-// For now I'm going to spawn one container per runtime
+// Package runtime provides structures to deal with different runtime environemnts
 package runtime
 
-import(
-  "context"
+import (
+	"context"
+
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
 
-type Runtime struct{
-  Name string
-  Exec string
-  Tag string
-  Image string
-  ExistsLocally *bool
+// Runtime represents a runtime environment. Eg: Node.js, CPython, CRuby etc
+type Runtime struct {
+	Name          string
+	Exec          string
+	Tag           string
+	Image         string
+	ExistsLocally *bool
 }
 
-func(r Runtime) CheckIfExistsLocally(cli *client.Client, ctx context.Context) bool{
+// CheckIfExistsLocally checks if the desired image is already present in the host machine
+func (r Runtime) CheckIfExistsLocally(ctx context.Context, cli *client.Client) bool {
 
-  if r.ExistsLocally != nil{
-    return *r.ExistsLocally
-  }else{
-    exists := true
-    AllImagesInHost, err := cli.ImageList(ctx, types.ImageListOptions{All: true})
-    if err != nil {
-      panic(err)
-    }
+	if r.ExistsLocally != nil {
+		return *r.ExistsLocally
+	}
+	exists := true
+	AllImagesInHost, err := cli.ImageList(ctx, types.ImageListOptions{All: true})
+	if err != nil {
+		panic(err)
+	}
 
-    for _, image := range AllImagesInHost{
-        for _, tag := range image.RepoTags{
-          if r.Tag == tag{
-            r.ExistsLocally = &exists
-            break
-          }
-        }
+	for _, image := range AllImagesInHost {
+		for _, tag := range image.RepoTags {
+			if r.Tag == tag {
+				r.ExistsLocally = &exists
+				break
+			}
+		}
 
-        if r.ExistsLocally != nil{
-          if *r.ExistsLocally == true{
-            break
-          }
-        }
-    }
+		if r.ExistsLocally != nil {
+			if *r.ExistsLocally == true {
+				break
+			}
+		}
+	}
 
-    if r.ExistsLocally == nil{
-      exists := false
-      r.ExistsLocally = &exists
-    }
-  }
+	if r.ExistsLocally == nil {
+		exists := false
+		r.ExistsLocally = &exists
+	}
 
-  return *r.ExistsLocally
+	return *r.ExistsLocally
 
 }
 
-var Runtimes = []Runtime {
-  Runtime{Name: "theangrydev_python", Tag: "python:latest", Exec: "python", Image: "docker.io/python"},
-  Runtime{Name: "theangrydev_ruby", Tag: "ruby:latest", Exec: "ruby", Image: "docker.io/ruby"},
-  Runtime{Name: "theangrydev_node", Tag: "node:latest", Exec: "node", Image: "docker.io/node"}}
+// Runtimes is a list of runtime environments we want to spawn to service requests
+// Currently only 1 runtime per language should suffice
+var Runtimes = []Runtime{
+	Runtime{Name: "theangrydev_python", Tag: "python:latest", Exec: "python", Image: "docker.io/python"},
+	Runtime{Name: "theangrydev_ruby", Tag: "ruby:latest", Exec: "ruby", Image: "docker.io/ruby"},
+	Runtime{Name: "theangrydev_node", Tag: "node:latest", Exec: "node", Image: "docker.io/node"}}
